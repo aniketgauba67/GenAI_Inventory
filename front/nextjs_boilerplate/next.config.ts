8fa6e2c3ffffba12b13c507bfb7e49f3af06f4a9
liamkaz <143825535+liamkaz@users.noreply.github.com>
2026-02-27 14:25:14 -0500

    attempted to export page for github pages use

--- front/nextjs_boilerplate/next.config.ts
+++ front/nextjs_boilerplate/next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/GenAI_Inventory",
+  assetPrefix: "/GenAI_Inventory/",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
